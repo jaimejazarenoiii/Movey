757b5d2c60ab675f34136e8c19e2fd67957f40d2
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2023-03-29 17:20:53 +0800

    Initial commit

---
+++ Movey/Scenes/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movey
+//
+//  Created by Jaime Jazareno III on 28/3/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
