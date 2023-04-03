f39976431d71918e0fef800a4d03685d69943577
Jaime Jazareno IV <jaimejazarenoiii@gmail.com>
2023-04-03 12:09:31 +0800

    Initial setup for view model

--- Movey/Scenes/Main/MainViewController.swift
+++ Movey/Scenes/Main/MainViewController.swift
@@ -17,6 +17,7 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupBindings()
     }
 
     private func setupBindings() {
--- Movey/Scenes/Main/MainViewModel.swift
+++ Movey/Scenes/Main/MainViewModel.swift
@@ -7,14 +7,17 @@
 
 import RxCocoa
 import RxSwift
+import UIKit
 
 protocol MainViewModelInputs {
     func viewDidLoad()
+    func set(collectionView: UICollectionView)
 }
 
 protocol MainViewModelOutputs {
     var status: BehaviorRelay<Bool?> { get }
     var error: BehaviorRelay<Error?> { get }
+    var tracks: BehaviorRelay<[Track]?> { get }
 }
 
 protocol MainViewModelTypes {
@@ -22,16 +25,34 @@ protocol MainViewModelTypes {
     var outputs: MainViewModelOutputs { get }
 }
 
-struct MainViewModel: MainViewModelTypes, MainViewModelOutputs, MainViewModelInputs {
+struct MainCollectionViewCellData {
+    let section: Int
+    let tracks: [Track]
+}
+
+class MainViewModel: MainViewModelTypes, MainViewModelOutputs, MainViewModelInputs {
     var inputs: MainViewModelInputs { return self }
     var outputs: MainViewModelOutputs { return self }
 
     var status: RxRelay.BehaviorRelay<Bool?>
     var error: RxRelay.BehaviorRelay<Error?>
+    var tracks: BehaviorRelay<[Track]?>
+
+    private let disposeBag: DisposeBag = DisposeBag()
+    private let trackService: TrackServiceable
 
     init () {
         status = .init(value: nil)
         error = .init(value: nil)
+        tracks = .init(value: nil)
+        trackService = TrackService()
+
+        viewDidLoadProperty.subscribe(onNext: { [weak self] _ in
+            guard let self else { return }
+            Task {
+                await self.fetchAllTracks()
+            }
+        }).disposed(by: disposeBag)
     }
 
     private let viewDidLoadProperty: PublishSubject<Void> = .init()
@@ -39,5 +60,13 @@ struct MainViewModel: MainViewModelTypes, MainViewModelOutputs, MainViewModelInp
         viewDidLoadProperty.onNext(())
     }
 
+    private func fetchAllTracks() async {
+        do {
+            try await trackService.fetchAllTrack()
+
+        } catch(let err) {
+            print("There's an error: \(err.localizedDescription)")
+        }
+    }
 
 }
