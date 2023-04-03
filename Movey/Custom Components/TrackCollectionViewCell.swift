a3a5e90d4db720961bb6493ad8b716c37b91cd60
Jaime Jazareno IV <jaimejazarenoiii@gmail.com>
2023-04-03 12:08:34 +0800

    Initial setup for the collection view cell

---
+++ Movey/Custom Components/TrackCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrackCollectionViewCell.swift
+//  Movey
+//
+//  Created by Jaime Jazareno IV on 3/29/23.
+//
+
+import Foundation
+import UIKit
+
+class TrackCollectionViewCell: UICollectionViewCell {
+
+}
