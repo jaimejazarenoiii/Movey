14f1ec8fd807c9e6096f77fb340d3f0d47d01e58
Jaime Jazareno IV <jaimejazarenoiii@gmail.com>
2023-04-03 12:08:59 +0800

    Add models

--- Movey/Models/Track.swift
+++ Movey/Models/Track.swift
@@ -11,24 +11,23 @@ class Track: Object, Codable {
     @Persisted(primaryKey: true) var trackId: Int
     @Persisted var wrapperType: String
     @Persisted var kind: String
-    @Persisted var collectionId: Int
+    @Persisted var collectionId: Int?
     @Persisted var artistName: String
-    @Persisted var collectionName: String
+    @Persisted var collectionName: String?
     @Persisted var trackName: String
-    @Persisted var collectionCensoredName: String
+    @Persisted var collectionCensoredName: String?
     @Persisted var trackCensoredName: String
-    @Persisted var collectionArtistId: String
-    @Persisted var isFavorite: Bool
-    @Persisted var collectionPrice: Double
-    @Persisted var trackPrice: Double
-    @Persisted var trackRentalPrice: Double
-    @Persisted var collectionHdPrice: Double
-    @Persisted var trackHdPrice: Double
-    @Persisted var trackHdRentalPrice: Double
-    @Persisted var releaseDate: Date
+    @Persisted var collectionArtistId: Int?
+    @Persisted var isFavorite: Bool? = false
+    @Persisted var collectionPrice: Double?
+    @Persisted var trackPrice: Double?
+    @Persisted var trackRentalPrice: Double?
+    @Persisted var collectionHdPrice: Double?
+    @Persisted var trackHdPrice: Double?
+    @Persisted var trackHdRentalPrice: Double?
     @Persisted var currency: String
-    @Persisted var shortDescription: String
+    @Persisted var shortDescription: String?
     @Persisted var primaryGenreName: String
-    @Persisted var longDescription: String
+    @Persisted var longDescription: String?
     @Persisted var artworkUrl100: String
 }
---
+++ Movey/Models/TrackResponse.swift
@@ -0,0 +1,12 @@
+//
+//  TrackResponse.swift
+//  Movey
+//
+//  Created by Jaime Jazareno IV on 3/29/23.
+//
+
+import Foundation
+
+struct TrackResponse: Codable {
+    var results: [Track]
+}
