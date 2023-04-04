c254d5cdad107893da820cbb6bc8f8812c2426e5
Jaime Jazareno IV <jaimejazarenoiii@gmail.com>
2023-04-05 00:24:17 +0800

    Create collection view cell data for diffable

---
+++ Movey/Models/TracksCollectionViewCellData.swift
@@ -0,0 +1,13 @@
+//
+//  TracksCollectionViewCellData.swift
+//  Movey
+//
+//  Created by Jaime Jazareno IV on 4/3/23.
+//
+
+import Foundation
+
+struct TracksCollectionViewCellData: Hashable {
+    let section: Int
+    let tracks: [Track]
+}
