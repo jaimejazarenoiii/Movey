ad0a149686204486c347dd5caa6f8f0c8411824d
Jaime Jazareno IV <jaimejazarenoiii@gmail.com>
2023-04-05 00:23:53 +0800

    Update custom components

---
+++ Movey/Custom Components/TrackCollectionViewCell/TrackCollectionViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  TrackCollectionViewCell.swift
+//  Movey
+//
+//  Created by Jaime Jazareno IV on 3/29/23.
+//
+
+import Foundation
+import Kingfisher
+import RxCocoa
+import RxSwift
+import UIKit
+
+protocol TrackCollectionViewCellDelegate: AnyObject {
+    func didTapFavoriteButton(isFavorite: Bool, indexPath: IndexPath)
+}
+
+class TrackCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    private let disposeBag: DisposeBag = DisposeBag()
+    private var indexPath: IndexPath?
+    weak var delegate: TrackCollectionViewCellDelegate?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupFavoriteButton()
+        setupBindings()
+    }
+
+    private func setupFavoriteButton() {
+        favoriteButton.setImage(UIImage(systemName: "heart.fill"), for: .selected)
+        favoriteButton.setImage(UIImage(systemName: "heart"), for: .normal)
+    }
+
+    private func setupBindings() {
+        favoriteButton.rx.controlEvent(.touchUpInside).subscribe(onNext: { [weak self] _ in
+            guard let self, let indexPath = self.indexPath else { return }
+            self.delegate?.didTapFavoriteButton(isFavorite: !self.favoriteButton.isSelected, indexPath: indexPath)
+        }).disposed(by: disposeBag)
+    }
+
+    func set(track: Track, indexPath: IndexPath) {
+        titleLabel.text = track.trackName
+        genreLabel.text = track.primaryGenreName
+        priceLabel.text = "$\(track.trackPrice ?? 0)"
+        backgroundImageView.kf.indicatorType = .activity
+        backgroundImageView.kf.setImage(with: URL(string: track.artworkUrl100))
+        favoriteButton.isSelected = track.isFavorite ?? false
+
+        self.indexPath = indexPath
+    }
+}
